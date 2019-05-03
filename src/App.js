4273eba2e174d50d4e2b7025943b2e66b9a739c0
birukyemane <bir_yem@yahoo.com>
2019-05-03 20:29:52 +0300

    set up redux and basic app layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-     
+class App extends Component {
+  
+  render () {
+   return ( 
+    <div className="container">   
+      <header></header>  
+      <Router>
+        <Route exact path='/' component={Home} />
+        <Route  path='/fetch' render={(props)=><FetchUsers {...props} />}/>      
+      </Router> 
+      <footer></footer>
     </div>
-  );
+   );
+  }
 }
 
 export default App;
