09c507ca66c6cd9af59d5129b1c1228407ba6ed0
birukyemane <bir_yem@yahoo.com>
2019-05-07 11:55:27 +0300

    connected to API. it saved to database

--- src/components/Stat.js
+++ src/components/Stat.js
@@ -1,12 +1,33 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
+import { API } from "aws-amplify";
+
+const handleSave = async event => {
+  try {
+    await createNote({"PickupLocation":{"Latitude":47.6174755835663,"Longitude":-122.28837066650185}});
+  } catch (e) {
+    alert(e);
+  }
+}
+
+const createNote = (note)=> {
+  return API.post("priceffTask", "/createstat", {
+    body: note
+  });
+}
+
 
 const Stat = ({ stat }) => {
   if(Object.keys(stat).length!==0){
     const {averageAge, oldestPerson, youngestPerson, northernMostPerson ,southernMostPerson} = stat;
-    return (<p>
+    return (
+    <div>
+         <p>
     {averageAge} {oldestPerson.name.first} {youngestPerson.name.first} {northernMostPerson.name.first} {southernMostPerson.name.first}
-     </p>)
+     </p>
+     <button onClick={handleSave}>Save</button>
+    </div>
+   )
   }else {
     return <p></p>
   }  
--- src/index.js
+++ src/index.js
@@ -5,11 +5,38 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import usersStat from './reducers';
+import Amplify from "aws-amplify";
+import config from "./config";
 
 const store = createStore(usersStat);
 store.subscribe(()=>{ // for testing redux
   console.log('store changed to',store.getState())
 })
+
+Amplify.configure({
+  /*
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },*/
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    //identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "priceffTask",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
+  }
+});
 
 ReactDOM.render(
     <Provider store={store}>
