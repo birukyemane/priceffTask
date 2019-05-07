0f3a3602ff8f17a5b795b26b4b314ee61e00fd19
birukyemane <bir_yem@yahoo.com>
2019-05-07 11:10:43 +0300

    set up config file

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+    s3: {
+      REGION: "eu-west-2",
+      BUCKET: "priceff-task"
+    },
+    apiGateway: {
+      REGION: "eu-west-2",
+      URL: "https://nrfajc3pbg.execute-api.eu-west-2.amazonaws.com/prod"
+    },
+    /*
+    cognito: {
+      REGION: "YOUR_COGNITO_REGION",
+      USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
+      APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
+      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    }*/
+  };
+  
